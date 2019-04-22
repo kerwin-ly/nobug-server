06975189714a59300f1ad23fb6a0bd3317ad1d7d
kerwin-ly <879688355@qq.com>
2019-04-22 23:01:44 +0800

    Add login api

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const { query } =  require('../utils/sql/query');
 
 module.exports = {
   register: async(userInfo) => {
-    let sql = 'INSERT INTO user (user_name, user_password, user_email, user_avatar) VALUES (?, ?, ?, ?)';
+    const sql = 'INSERT INTO user (user_name, user_password, user_email, user_avatar) VALUES (?, ?, ?, ?)';
     let values = [userInfo.name, userInfo.password, userInfo.email, userInfo.avatar];
     let data;
 
@@ -21,10 +21,26 @@ module.exports = {
     return data;
   },
   login: async (loginInfo) => {
-    
+    const sql = 'SELECT * FROM user WHERE user_name = ? AND user_password = ?';
+    let values = [loginInfo.username, loginInfo.password];
+    let data;
+
+    try {
+      await query(sql, values);
+      data = {
+        status: 200,
+        message: '登录成功'
+      }
+    } catch (error) {
+      data = {
+        status: 500,
+        message: error
+      }
+    }
+    return data;
   },
   getUserList: async () => {
-    let sql = 'SELECT * FROM user';
+    const sql = 'SELECT * FROM user';
     let data;
 
     try {
--- router/user.js
+++ router/user.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const userController = require('../controller/user');
 
+router.post('/login', userController.login);
 router.post('/register', userController.register);
 router.get('/list', userController.getUserList);
 
