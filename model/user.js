dba039ec1a59e3522d41af15e52af1b89ff6a563
kerwin-ly <879688355@qq.com>
2019-04-24 12:02:38 +0800

    Add register api

--- model/user.js
+++ model/user.js
@@ -2,16 +2,15 @@ const { query } =  require('../utils/sql/query');
 
 module.exports = {
   login: async (loginInfo, ctx) => {
-    const sql = 'SELECT user_id, user_name FROM user WHERE user_name = ? AND user_password = ?';
-    const values = [loginInfo.username, loginInfo.password];
+    const sql = 'SELECT user_id FROM user WHERE user_email = ? AND user_password = ?';
+    const values = [loginInfo.email, loginInfo.password];
     const result = await query(sql, values);
 
     if ( Array.isArray(result) && result.length > 0 ) {
       ctx.session.user = result[0];
       return {
         status: 200,
-        message: '操作成功',
-        data: result[0]
+        message: '操作成功'
       }
     }
     return {
@@ -20,15 +19,23 @@ module.exports = {
     }
   },
   register: async (userInfo) => {
-    const sql = 'INSERT INTO user (user_name, user_password, user_email, user_avatar) VALUES (?, ?, ?, ?)';
-    const values = [userInfo.name, userInfo.password, userInfo.email, userInfo.avatar];
-    const result = await query(sql, values);
+    const checkExitUserSql = `SELECT user_id from user WHERE user_email= "${userInfo.email}"`;
+    const exitUser = await query(checkExitUserSql);
 
-    if ( Array.isArray(result) && result.length > 0 ) {
-      ctx.session.user = result[0];
+    if (Array.isArray(exitUser) && exitUser.length > 0) {
+      return {
+        status: 207,
+        message: '当前用户已存在'
+      }
+    }
+
+    const sql = `INSERT INTO user (user_name, user_email, user_password) VALUES ("${userInfo.name}", "${userInfo.email}", "${userInfo.password}")`;
+    const result = await query(sql);
+
+    if (result && result.insertId >= 0) {
       return {
         status: 200,
-        message: '操作成功'
+        message: '注册成功'
       }
     }
   },
